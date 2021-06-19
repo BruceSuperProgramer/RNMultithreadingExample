6ac273fa6a4db667c44a3e5e06b60537441e69ae
Bruce He <bruce.he@hammertech.com.au>
2021-06-20 03:41:34 +1000

    save

--- src/persistors/realm/realm.js
+++
@@ -1,4 +0,0 @@
-import Realm from 'realm';
-import {databaseOptions} from './configs';
-
-export default new Realm(databaseOptions);
--- src/screens/home/HomeContainer.js
+++ src/screens/home/HomeContainer.js
@@ -5,20 +5,29 @@ import HomeScreen from './HomeScreen';
 
 class HomeContainer extends React.Component {
   componentDidMount() {
-    nodejs.start('loadInspections.js');
-    nodejs.channel.addListener(
-      'message',
-      msg => {
-        alert(msg);
-      },
-      this,
-    );
-
     console.log('realm.path:', realm.path);
+    nodejs.start('loadInspections.js');
+    nodejs.channel.addListener('message', inspections => {}, this);
   }
 
+  onLoadDataFromWorkerPress = () => {
+    nodejs.channel.send('starting fetch API data');
+  };
+
+  onLoadDataFromMainThreadrPress = async () => {
+    try {
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   render() {
-    return <HomeScreen />;
+    return (
+      <HomeScreen
+        onLoadDataFromWorkerPress={this.onLoadDataFromWorkerPress}
+        onLoadDataFromMainThreadrPress={this.onLoadDataFromMainThreadrPress}
+      />
+    );
   }
 }
 
